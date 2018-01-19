0567857eddd4e95973a1a71bb8e307bf3c06a5a0
Ankita Gehalot <gehalotankita@gmail.com>
2018-01-19 21:58:56 +0530

    adding task one in server.js

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var axios = require('axios');
 var app = express();
 var router = express.Router();
 
@@ -7,6 +8,95 @@ var server = require('http').Server(app);
 app.get('/', function(req, res) {
   res.send('Hello World-Ankita');
 });
+
+app.get("/setcookie", function(req, res){
+  res.cookie('name', 'ankita');
+  res.cookie('age',25);
+  res.end();
+});
+
+app.get("/getcookie", function(req,res){
+   console.log(req.headers.cookie);
+   res.send(req.headers.cookie);
+});
+
+app.get("/robots.txt", function(req, res){
+  res.status(403).end();
+})
+
+app.get("/html", function(req,res){
+  res.send(
+      `
+      <html>
+          <body>
+              <div>
+                  <h1>First Page</h1>
+                  <p>This is my first paragraph in html</p>
+              </div>
+          </body>
+      </html>
+      `
+      )
+});
+
+app.get("/input",function(req,res){
+  res.send(
+     ` 
+      <html>
+      <body>
+          <h1>Form Submission</h1>
+          <form  method="post">
+              <input name="UserName" type="text"/>
+              <input type="submit" value="OK"/>
+          </form>
+      </body>
+      </html>
+     `);
+});
+
+app.post("/input",function(req,res){
+  res.send(
+     ` 
+      <html>
+      <body>
+          <h1>Recieved</h1>
+      </body>
+      </html>
+     `);
+  console.log(req.body.UserName);
+});
+
+app.get("/authors", function(req,res){
+  axios
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then(function(usersResponse) {
+      axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function(postsResponse){
+          console.log(usersResponse.data);
+          console.log(postsResponse.data);
+          var users = usersResponse.data;
+          var posts = postsResponse.data;
+
+          var authors = '';
+          for(i = 0; i < users.length; i++) {
+              var currentUser = users[i];
+
+              var count = 0;
+              for(k = 0; k < posts.length; k++) {
+                  if(posts[k].userId == currentUser.id) {
+                      count = count + 1;
+                  }
+              }
+
+              authors = authors + '\r\n' + currentUser.name + ' ' +  count;
+              
+          }
+
+          res.send(authors);
+      });
+  })
+})
 
 // Uncomment to add a new route which returns hello world as a JSON
 app.get('/api/winloss', function(req, res) {
